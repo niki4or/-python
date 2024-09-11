d721cdf1e08a65e7fbbbec75f142cf9f63ee6681
niki4or <niki4rof@yandex.ru>
2024-09-11 08:55:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-#"1st program"
-print((9**0.5)*5)
-#"2nd program"
-print(9.99 > 9.98 and 1000 != 1000.1 )
-#"3rd program"
-print(2*2+2)
-print(2*(2+2))
-print(6==8)
-#"4th program"
-print(int((123.456*10)%10))
+name="international"
+print(name[0])
+print(name[-1])
+print(name[6:])
+print(name[::-1])
+print(name[0::2])
