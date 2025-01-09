e85beec0566f252664a9ee4e801239cf081bc2a3
niki4or <niki4rof@yandex.ru>
2025-01-09 22:13:18 +0300

    Add files via upload

--- module_1_7.py
+++ module_1_7.py
@@ -2,8 +2,8 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
     if my_list[index] < 0:
-            break
+            continue
     if my_list[index] > 0:
-                print(my_list[index])
-                index += 1
+        print(my_list[index])
+    index += 1
 
